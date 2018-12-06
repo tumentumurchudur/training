125db1af83593721ed5edeefaced3d48ffe89aa1
AMAIISDOM\ttumurchudur <tumen_23@yahoo.com>
2018-12-05 22:48:25 -0700

    Find median, mean and mode from array of numbers

--- Asked/Dish/find-median-two-sorted-arrays.js
+++ Asked/Dish/find-median-two-sorted-arrays.js
@@ -1,22 +1,56 @@
 // Find median of two sorted array of different lengths
 function medianOfTwoSortedArrays(x, y) {
-	const z = x.concat(y).sort((a, b) => {
-		if (a > b) return 1;
-		if (a < b) return -1;
-		return 0;
-	});
+	const z = x.concat(y).sort((a, b) => a - b);
+	const mid = Math.floor(z.length / 2);
 
-	// Check if # of elements is even.
-	if (z.length % 2 === 0) {
-  	const n = z.length / 2;
-
-  	return (z[n] + z[n - 1]) / 2;
-	} else {
-  	const n = (z.length - 1) / 2;
-
-  	return z[n];
-	}
+	return z.length % 2 === 0 ? (z[mid] + z[mid - 1]) / 2 : z[mid];
 }
 
-// even [1, 3, 5, 10]
-// odd  [1, 3, 5, 10, 15]
+function Statistics() {
+  return {
+    median(nums) {
+      const sortedNums = [...nums.sort((a, b) => a - b)];
+      const mid = Math.floor(sortedNums.length / 2);
+
+      return sortedNums.length % 2 === 0 ?
+        (sortedNums[mid - 1] + sortedNums[mid]) / 2 :
+        sortedNums[mid];
+    },
+    mean(nums) {
+      return nums.reduce((x, y) => x + y) / nums.length;
+    },
+    mode(nums) {
+      let dict = {};
+
+      for (const num of nums) {
+        if (dict[num]) {
+          dict[num]++;
+        } else {
+          dict[num] = 1;
+        }
+      }
+
+      let maxVal = Number.MIN_SAFE_INTEGER;
+      let maxKey = null;
+
+      for (const key in dict) {
+        if (dict[key] > maxVal) {
+          maxVal = dict[key];
+          maxKey = key;
+        }
+      }
+
+      return { key: maxKey, value: maxVal };
+    }
+  };
+}
+
+let x = [12, 3, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+let stat = new Statistics();
+
+console.log(`Median: ${stat.median(x)}`);
+
+console.log(`Mean: ${stat.mean(x)}`);
+
+const mode = stat.mode(x);
+console.log(`Mode: key:${mode.key}, value:${mode.value}`)
